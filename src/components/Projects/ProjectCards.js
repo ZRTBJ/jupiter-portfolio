cc901fe9ad1d2e2d07ad8722280ca671c9ede329
ZRTBJ <superstar753953@gmail.com>
2025-01-24 10:11:59 -0500

    Fixing build errors

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -10,12 +7,11 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{width: 400, height:250}}/>
       <Card.Body>
         <Card.Title style={{fontWeight: 'bold'}}>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify", textAlign: 'center' }}>
+        <Card.Text style={{ textAlign: 'center' }}>
           {props.description}
         </Card.Text>
         {"\n"}
         {"\n"}
-
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
       </Card.Body>
     </Card>
